9f0bdac86a4b8f4a32c062035f0145594c52000f
ShadowRambler <zhangbohan@jd.com>
2016-11-08 21:34:25 +0800

    update upload

--- src/backend/component/CustomUpload.jsx
+++ src/backend/component/CustomUpload.jsx
@@ -5,45 +5,85 @@ import {Button, Upload, Icon} from 'antd';
 class CustomUpload extends Component {
     constructor(props){
         super(props);
-        let fileList = [];
-        if(props.value){
-            fileList.push({
-                uid: -1,
-                name: props.value,
-                status: 'done',
-                url: props.value,
-            });
-        }
-       this.state ={
-           fileList
-       }
+        this.state = this.mapValueToFileList(props.value);
     }
     static propTypes = {
-        value: PropTypes.string,
         onChange: PropTypes.func,
     }
+    componentWillReceiveProps(nextProps){
+        console.warn('componentWillReceiveProps',nextProps.value,this.props.value);
+        if(nextProps.value!=this.props.value){
+            this.setState(this.mapValueToFileList(nextProps.value));
+        }
+    }
+    mapValueToFileList(value){
+        let fileList = [];
+        let isList = false;
+        if(Array.isArray(value)){
+             value.forEach((item,index)=>{
+                fileList.push({
+                    uid: (-index)+'',
+                    name: item,
+                    status: 'done',
+                    url: item,
+                })
+            })
+            isList = true;
+
+        }
+        else if(value){
+            fileList.push({
+                uid: -1,
+                name: value,
+                status: 'done',
+                url: value,
+            });
+
+        }
+        return {
+            fileList,
+            isList
+        }
+    }
     onChange(e){
+        console.log('onChange',e);
+
         let fileList = e.fileList;
-        fileList = fileList.slice(-1);
-        this.setState({
-            fileList
-        })
-        if(e.file.status == 'removed'){
-            this.props.onChange(null)
+
+        if(this.state.isList){
+            if(e.file.status == 'removed'){
+                this.props.onChange(null)
+            }
+            else if(e.file.status == 'done'){
+                this.props.onChange(fileList.map((item)=>item.url))
+            }
         }
-        else if(e.file.status == 'done'){
-            this.props.onChange(e.file.response.response.data.url)
+        else {
+            fileList = fileList.slice(-1);
+
+            if(e.file.status == 'removed'){
+                this.props.onChange(null)
+            }
+            else if(e.file.status == 'done'){
+                this.props.onChange(e.file.response.response.data.url)
+            }
+            this.setState({
+                fileList
+            })
         }
+
 
     }
     render(){
         const { fileList } = this.state;
         const props = {
+            name:'image',
             action: '/api/upload',
             listType: 'picture',
             // listType: 'picture-card',
             fileList
         };
+
         return (
             <Upload {...props} className="upload-list-inline" onChange={(e)=>this.onChange.call(this,e)}>
                 <Button type="ghost">
@@ -53,5 +93,6 @@ class CustomUpload extends Component {
         )
     }
 }
+
 
 export default CustomUpload;
