be95259aae8c8a03c256a2de529deba5a424776b
ShadowRambler <zhangbohan@jd.com>
2016-11-18 17:56:10 +0800

    update upload

--- src/backend/reducers/order/index.js
+++ src/backend/reducers/order/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import item from './item';
 import list from './list';
+import cancel from './cancel';
 
 
 
 const orderReducer = combineReducers({
   item,
   list,
+  cancel,
 });
 
 export default orderReducer;
